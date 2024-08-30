54b6b113e1c76ee9b344dc4b34b7080cd0a4d6d8
nany9 <msx009@mail.ru>
2024-08-31 01:52:03 +0300

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const tg = window.Telegram.WebApp;
+const contact_btn = document.getElementById("share-btn");
+const phone_input = document.getElementById("phone-input");
 document.getElementById("query").textContent = tg.initDataUnsafe?.user?.username;
+// document.getElementById("phone-input").value = "+79042014558";
 tg.MainButton.text = 'Купить';
 tg.MainButton.show();
+
+contact_btn.addEventListener('click', () => {
+    tg.requestContact(function(phone){
+        phone_input.value = phone;
+    });
+});
