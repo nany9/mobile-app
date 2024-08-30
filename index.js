159e33dfefcf71b6de501e89f90875cdb0147f65
nany9 <msx009@mail.ru>
2024-08-31 02:25:49 +0300

    removed unneccessary lines

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const tg = window.Telegram.WebApp;
 const contact_btn = document.getElementById("share-btn");
 const phone_input = document.getElementById("phone-input");
 document.getElementById("query").textContent = tg.initDataUnsafe?.user?.username;
-// document.getElementById("phone-input").value = "+79042014558";
 tg.MainButton.text = 'Купить';
 tg.MainButton.show();
 
 contact_btn.addEventListener('click', () => {
     tg.requestContact(function(status, data){
-        alert(status);
-        console.log('status',status);
         const phone = data.responseUnsafe?.contact?.phone_number;
-        console.log('nomer:', phone);
         phone_input.value = phone;
     });
 });
