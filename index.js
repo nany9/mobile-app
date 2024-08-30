c1d447744858a0228723617573a4154edcd8cb47
nany9 <msx009@mail.ru>
2024-08-30 22:22:28 +0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const tg = window.Telegram.WebApp;
 
-tg.MainButton.text = 'Купить';
-tg.MainButton.show();
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = '6874076326:AAHrQxJs8jZzZc6O2lU3h6X2MSxPSwdHJfI';
+const bot = new TelegramBot(TOKEN, {
+    polling: true
+});
+
+bot.on('text', async msg => {
+    await bot.sendMessage(msg.chat.id, 'Привет')
+});
