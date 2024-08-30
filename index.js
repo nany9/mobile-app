40cc114653c622de6c21a6008ec196136a19fd2c
nany9 <msx009@mail.ru>
2024-08-30 23:45:19 +0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const tg = window.Telegram.WebApp;
-document.getElementById("query").textContent = tg.initDataUnsafe?;
+document.getElementById("query").textContent = tg.initDataUnsafe;
 tg.MainButton.text = 'Купить';
 tg.MainButton.show();
