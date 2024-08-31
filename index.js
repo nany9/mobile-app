7e02bd14405754e667b7e2a7f6e227d926ecd9e4
nany9 <msx009@mail.ru>
2024-08-31 05:09:45 +0300

    phone view

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ document.getElementById("query").textContent = tg.initDataUnsafe?.user?.username
 tg.MainButton.text = 'Купить';
 tg.MainButton.show();
 
+function returnPhone(str){
+    var out = "";
+    out += '+' + str[0] + ' ';
+    out += str.slice(1,4) + ' ';
+    out += str.slice(4,7) + ' ';
+    out += str.slice(7,9) + ' ';
+    out += str.slice(9,11) + ' ';
+    return out
+}
+
 contact_btn.addEventListener('click', () => {
     tg.requestContact(function(status, data){
         const phone = data.responseUnsafe?.contact?.phone_number;
         if (typeof(phone) === "undefined"){
             phone_input.value = "Нет доступа";
         } else {
-            phone_input.value = phone;
+            phone_input.value = returnPhone(phone);
         }
     });
 });
