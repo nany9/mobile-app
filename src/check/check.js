0fd214ba399ffe6aa1ceb94237b222cfbe7002b5
nany9 <msx009@mail.ru>
2024-09-01 06:18:32 +0300

    fixed location bug #2

--- src/check/check.js
+++ src/check/check.js
@@ -1,6 +1,6 @@
 const tg = window.Telegram.WebApp;
 
 tg.BackButton.onClick(function(){
-    window.location.href='https://nany9.github.io/mobile-app/index.html';
+    window.location.href='https://nany9.github.io/mobile-app/';
     tg.BackButton.hide();
 });
